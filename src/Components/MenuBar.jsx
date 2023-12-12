ccb69f5ddb84766c7ad11744a3426ef4c45ca98d
imadbenmadi <imadbenmadi891@gmail.com>
2023-12-12 17:31:59 +0100

    Adding Animation to navbar

--- src/Components/MenuBar.jsx
+++ src/Components/MenuBar.jsx
@@ -12,33 +12,41 @@ export default function MenuBar() {
                 className="md:hidden bg-Blue w-[100px] h-[100px] rounded-br-[80px] absolute top-[-25px] left-[-35px] z-10"
                 onClick={toggleMobileMenu}
             ></div>
-            <div className=" flex w-full">
+            <div className="md:hidden flex w-full">
                 <div
-                    className={`${MobileMenuVisible ? "block" : "hidden"}
-           md:hidden bg-blue-500 w-[65%] h-[100vh] z-50 relative`}
+                    className={`${
+                        MobileMenuVisible
+                            ? "block w-[65%] translate-x-0"
+                            : "hidden w-0 translate-x-full"
+                    } md:hidden bg-blue-500 h-[100vh] z-50 relative transition-all duration-300 ease-in-out`}
                 >
                     <div
-                        className=" w-5 h-5 bg-white text-Blue font-bold rounded flex justify-center items-center  absolute right-2 top-2 "
+                        className="w-5 h-5 bg-white text-Blue font-bold rounded flex justify-center items-center absolute right-2 top-2"
                         onClick={toggleMobileMenu}
                     >
                         X
                     </div>
-                    <div className=" flex flex-col items-center justify-center h-[100%]">
+                    <div className="flex flex-col items-center justify-center h-[100%]">
                         <div>Home</div>
                         <div>Home</div>
                         <div>Home</div>
                         <div>Home</div>
                     </div>
                 </div>
+
                 <div
-            className={`${MobileMenuVisible ? "block" : "hidden"} w-[35%] h-[100vh] bg-gray-400`}
+                    className={`${
+                        MobileMenuVisible
+                            ? "bg-gray-400 transition-all duration-100 ease-in-out"
+                            : "bg-transparent"
+                    } w-[35%] h-[100vh]`}
                     onClick={toggleMobileMenu}
                 ></div>
             </div>
 
             {/* Laptop Menu Bar */}
 
-            <div className=" hidden md:block    h-[100vh] bg-Blue w-[205px]"></div>
+            <div className=" hidden md:block    h-[100vh] bg-Blue w-[255px]"></div>
         </>
     );
 }
