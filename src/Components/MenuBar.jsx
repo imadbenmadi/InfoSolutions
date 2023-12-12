b0bed9ee51be9430db6acc323df1fcfd1b518ca0
imadbenmadi <imadbenmadi891@gmail.com>
2023-12-12 20:39:41 +0100

    Addding Elements into Menu Bar

--- src/Components/MenuBar.jsx
+++ src/Components/MenuBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 export default function MenuBar() {
     const [MobileMenuVisible, setMobileMenuVisibility] = useState(false);
     const toggleMobileMenu = () => {
@@ -10,18 +11,19 @@ export default function MenuBar() {
           <div className="md:hidden  ">
               {/* Mobile menu bar */}
               <div
-                  className=" bg-Blue w-[100px] h-[100px] rounded-br-[80px] absolute top-[-25px] left-[-35px]  z-10"
+                  className={`${
+                      MobileMenuVisible ? "hidden " : " block"
+                  } bg-Blue w-[100px] h-[100px] rounded-br-[80px] absolute top-[-25px] left-[-35px]  z-10`}
                   onClick={toggleMobileMenu}
               ></div>
 
-              {/* {MobileMenuVisible && ( */}
               <div className=" flex w-full fixed ">
                   <div
                       className={`${
                           MobileMenuVisible
                               ? " translate-x-[0%]"
                               : " translate-x-[-100%]"
-                      }  bg-Blue w-[65%] h-[100vh] z-20 relative transition-all duration-300 ease-in-out`}
+                      }  bg-Blue w-[65%] h-[100vh] z-30 relative transition-all duration-300 ease-in-out`}
                   >
                       <div
                           className="w-[40px] h-[40px] text-xl bg-white text-Blue font-bold rounded-bl-xl  rounded-tr-xl rounded-tl-sm  rounded-br-sm flex justify-center items-center absolute right-2 top-2"
@@ -29,24 +31,30 @@ export default function MenuBar() {
                       >
                           X
                       </div>
-                      <div className="flex flex-col items-center justify-center h-[100%]">
-                          <div>Home</div>
-                          <div>Home</div>
-                          <div>Home</div>
-                          <div>Home</div>
+                      <div className="flex flex-col items-start justify-center gap-7 h-[100%] text-white font-semibold ">
+                          <div className=" ml-3 border-b-2"><Link to={"/"}> Home</Link></div>
+                          <div className=" w-full h-1 bg-white"></div>
+                          <div className=" ml-3">Programation Linéer</div>
+                          <div className=" ml-3">Propabilité Statistique</div>
+                          <div className=" ml-3">Systéme D'exploitation</div>
+                          <div className=" w-full h-1 bg-white"></div>
+                          <div className=" ml-3">Théory Des Graphs</div>
+                          <div className=" ml-3">Théory des Langage</div>
+                          <div className=" ml-3">Méthodes Numérique</div>
+                          <div className=" w-full h-1 bg-white"></div>
+                          <div className=" ml-3">Systéme Machine</div>
                       </div>
                   </div>
 
                   <div
                       className={`${
                           MobileMenuVisible
-                              ? " translate-x-0 bg-gray-400 transition-colors delay-300 duration-200 ease-in-out"
+                              ? " translate-x-0 bg-gray-400 transition-colors delay-150 duration-300 ease-in-out"
                               : " translate-x-full bg-transparent"
                       } w-[35%] h-[100vh] opacity-[0.3] `}
                       onClick={toggleMobileMenu}
                   ></div>
               </div>
-              {/* )} */}
 
               {/* Laptop Menu Bar */}
           </div>
