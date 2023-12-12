566cae32fc9f165748cdaf6e76902420b9e9aeb6
imadbenmadi <imadbenmadi891@gmail.com>
2023-12-12 18:27:21 +0100

    fixing overflow

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -4,7 +4,7 @@ import Header from '../Header';
 export default function Home() {
     return (
         <>
-            <div className="relative h-fit ">
+            <div className="relative h-[100vh] max-h-[100vh] w-[100vw] overflow-hidden ">
                 <MenuBar />
                 <Header />
             </div>
