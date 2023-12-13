5220e40ffc5830cb9308be06dd1d86027221bd5f
imadbenmadi <imadbenmadi891@gmail.com>
2023-12-13 20:12:55 +0100

    changing header Format

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,19 +3,14 @@ import logo from "../assets/Cntic_Logo.png";
 
 export default function Header() {
     return (
-        <div className="text-center flex justify-center w-full flex-col md:flex-row md:gap-4  ">
-            <span className="  text-Blue text-4xl font-semibold md:order-2 md:mt-2">
+        <div className="text-center flex justify-end flex-row w-full   md:gap-4 sm:justify-center md:justify-center ">
+            <img src={logo} alt="Logo" className=" w-[100px] 	md:mr-5 md:m-0" />
+            <span className="  text-Blue text-2xl font-semibold md:order-2 md:mt-2 md:text-4xl">
                 INFO
             </span>
-            <span className=" text-4xl text-Black font-semibold md:order-3 md:mt-2">
+            <span className=" text-2xl text-Black font-semibold md:order-3 md:mt-2 md:text-4xl">
                 Solutions
             </span>
-
-            <img
-                src={logo}
-                alt="Logo"
-                className=" w-[150px] md:order-1 m-auto	md:mr-5 md:m-0"
-            />
         </div>
     );
 }
