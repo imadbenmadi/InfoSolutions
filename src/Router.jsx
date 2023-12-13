7d842dee168402ae6afb470abfeb79cc9cf24a4a
imadbenmadi <imadbenmadi891@gmail.com>
2023-12-13 20:22:40 +0100

    init NotFoundd Route

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "./Components/Home";
 import App from "./App";
 import PL from "./Components/Apps/PL";
+import NotFound from "./Components/NotFound";
 const routes = createBrowserRouter([
     {
         path: "/",
@@ -10,8 +11,13 @@ const routes = createBrowserRouter([
         children: [
             { index: true, element: <Home /> },
             { path: "/PL", element: <PL /> },
+            {
+                path: "*",
+                element: <NotFound />,
+            },
         ],
     },
+    
 ]);
 
 export default routes;
