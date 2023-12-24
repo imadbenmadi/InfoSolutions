a265ba4533f8fd3f0a4fc6039690313dc2e23fac
imadbenmadi <imadbenmadi891@gmail.com>
2023-12-24 19:37:50 +0100

    .

--- src/Components/PL Components/Graph.jsx
+++ src/Components/PL Components/Graph.jsx
@@ -6,7 +6,7 @@ export default function Graph() {
     return (
         <div className=" flex flex-col items-center justify-center  h-fit">
             <Graph_Methodes method={method} setMethod={setMethod} />
-            <div className="  top-0 w-[90vw] md:w-[70vw] h-[300px] bg-black m-auto"></div>
+            <div className=" w-[90vw] md:w-[70vw] h-[300px] bg-black m-auto"></div>
         </div>
     );
 }
