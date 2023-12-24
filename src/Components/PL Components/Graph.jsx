41c4e2c01f15b6d49c81c637e61c7dafcf3b45aa
imadbenmadi <imadbenmadi891@gmail.com>
2023-12-24 19:21:41 +0100

    init Graph

--- src/Components/PL Components/Graph.jsx
+++ src/Components/PL Components/Graph.jsx
@@ -4,9 +4,9 @@ import Graph_Methodes from "./Graph_Methodes";
 export default function Graph() {
     const [method, setMethod] = useState("sommets");
     return (
-        <div className=" flex flex-col items-center justify-center">
+        <div className=" flex flex-col items-center justify-center  h-fit">
             <Graph_Methodes method={method} setMethod={setMethod} />
-            <div className=" w-[700px] h-[400px] bg-black m-auto"></div>
+            <div className="  top-0 w-[90vw] md:w-[70vw] h-[300px] bg-black m-auto"></div>
         </div>
     );
 }
