3de400fe9b35fa88a307d6b6c0f7f3d3ebca8dcb
imadbenmadi <imadbenmadi891@gmail.com>
2023-12-25 22:29:52 +0100

    Configuring The Graph

--- src/Components/PL Components/Graph.jsx
+++ src/Components/PL Components/Graph.jsx
@@ -34,20 +34,50 @@ const Graph = () => {
         });
 
         const layout = {
-            xaxis: { title: "X" },
-            yaxis: { title: "Y" },
+            xaxis: { range: [-1, 10] },
+            yaxis: { range: [-1, 10] },
+            showlegend: true, 
+            legend: {
+                x: 0.1, 
+                y: 1.1, 
+                xanchor: "center", // Anchor point for x-coordinate ('left', 'center', 'right')
+                yanchor: "top", // Anchor point for y-coordinate ('top', 'middle', 'bottom')
+            },
+            margin: { l: 0, r: 0, t: 50, b: 20 },
+            dragmode: false,
         };
-
+        const config = {
+            displayModeBar: true, // Hide the interactive mode bar
+            modeBarButtons: [
+                ["pan2d"], // Enable 2D panning
+                ["zoomIn2d"], // Zoom in and zoom out buttons
+                ["zoomOut2d"], // Zoom in and zoom out buttons
+                ["resetScale2d"], // Reset 2D zoom and pan
+                ["toImage"], // Download plot as an image
+            ],
+            displaylogo: false, // Hide the Plotly logo
+            responsive: true, // Enable responsiveness
+            editable: false, // Disable chart editing
+            scrollZoom: true, // Disable scroll zoom
+            showTips: false, // Show tips on hover
+            modeBarButtonsToRemove: ["select2d", "lasso2d"],
+            toImageButtonOptions: {
+                format: "png", // Image download format (can be 'png', 'jpeg', 'webp')
+                filename: "PL", // Default filename for downloaded image
+                height: 500, // Image height
+                width: 700, // Image width
+            },
+        };
         const plot = (
             <Plot
                 data={plotData}
                 layout={layout}
-                config={{ displayModeBar: false }}
-                style={{ width: "1000px", height: "500px" }}
+                config={config}
+                style={{ width: "90%", margin: "auto", height: "100%" }}
+                useResizeHandler={true}
+                autosize={true}
             />
         );
-        console.log("plotComponent:", plot);
-
         setPlotComponent(plot);
     }, [Constraints]);
 
@@ -56,7 +86,7 @@ const Graph = () => {
     };
 
     return (
-        <div className="mt-4">
+        <div className="mt-4 pb-20">
             <Graph_Methodes onMethodChange={handleMethodChange} />
             <div className="card mt-4">
                 <div id="constraint-plot" className="card-body">
