98196f67fed1565173f06ada355f865d09d5c197
imadbenmadi <imadbenmadi891@gmail.com>
2023-12-27 13:12:55 +0100

    reving hightlinght

--- src/Components/PL Components/Graph.jsx
+++ src/Components/PL Components/Graph.jsx
@@ -7,79 +7,87 @@ const Graph = () => {
     const { Constraints } = usePLContext();
     const [plotComponent, setPlotComponent] = useState(null);
 
-    useEffect(() => {
-        const plotData = Constraints.map((constraint, index) => {
-            const { PlusMinus1, PlusMinus2, Value, X1, X2, Operatore } =
-                constraint;
-            const operator = Operatore === ">=" ? "≥" : "≤";
+     useEffect(() => {
+         
 
-            return {
-                type: "scatter",
-                mode: "lines",
-                name: `Constraint ${index + 1}`,
-                x: [-1, 8],
-                y:
-                    PlusMinus2 === "+"
-                        ? [
-                              Value - (X1 * -1 + Number(X2) * -1),
-                              Value - (X1 * 8 + Number(X2) * 8),
-                          ]
-                        : PlusMinus2 === "-"
-                        ? [
-                              Value - (X1 * -1 - Number(X2) * -1),
-                              Value - (X1 * 8 - Number(X2) * 8),
-                          ]
-                        : null,
-            };
-        });
 
-        const layout = {
-            xaxis: { range: [-1, 10] },
-            yaxis: { range: [-1, 10] },
-            showlegend: true, 
-            legend: {
-                x: 0.1, 
-                y: 1.1, 
-                xanchor: "center", // Anchor point for x-coordinate ('left', 'center', 'right')
-                yanchor: "top", // Anchor point for y-coordinate ('top', 'middle', 'bottom')
-            },
-            margin: { l: 0, r: 0, t: 50, b: 20 },
-            dragmode: false,
-        };
-        const config = {
-            displayModeBar: true, // Hide the interactive mode bar
-            modeBarButtons: [
-                ["pan2d"], // Enable 2D panning
-                ["zoomIn2d"], // Zoom in and zoom out buttons
-                ["zoomOut2d"], // Zoom in and zoom out buttons
-                ["resetScale2d"], // Reset 2D zoom and pan
-                ["toImage"], // Download plot as an image
-            ],
-            displaylogo: false, // Hide the Plotly logo
-            responsive: true, // Enable responsiveness
-            editable: false, // Disable chart editing
-            scrollZoom: true, // Disable scroll zoom
-            showTips: false, // Show tips on hover
-            modeBarButtonsToRemove: ["select2d", "lasso2d"],
-            toImageButtonOptions: {
-                format: "png", // Image download format (can be 'png', 'jpeg', 'webp')
-                filename: "PL", // Default filename for downloaded image
-                height: 500, // Image height
-                width: 700, // Image width
-            },
-        };
-        const plot = (
-            <Plot
-                data={plotData}
-                layout={layout}
-                config={config}
-                style={{ width: "90%", margin: "auto", height: "100%" }}
-                useResizeHandler={true}
-                autosize={true}
-            />
-        );
-        setPlotComponent(plot);
-    }, [Constraints]);
+         const plotData = Constraints.map((constraint, index) => {
+             const { PlusMinus1, PlusMinus2, Value, X1, X2, Operatore } =
+                 constraint;
+             const operator = Operatore === ">=" ? "≥" : "≤";
+
+             return {
+                 type: "scatter",
+                 mode: "lines",
+                 name: `Constraint ${index + 1}`,
+                 x: [-1, 10],
+                 y:
+                     PlusMinus2 === "+"
+                         ? [
+                               Value - (X1 * -1 + Number(X2) * -1),
+                               Value - (X1 * 8 + Number(X2) * 10),
+                           ]
+                         : PlusMinus2 === "-"
+                         ? [
+                               Value - (X1 * -1 - Number(X2) * -1),
+                               Value - (X1 * 10 - Number(X2) * 10),
+                           ]
+                         : null,
+             };
+         });
+
+         
+
+         const layout = {
+             xaxis: { range: [-1, 10] },
+             yaxis: { range: [-1, 10] },
+             showlegend: true,
+             legend: {
+                 x: 0.1,
+                 y: 1.1,
+                 xanchor: "center",
+                 yanchor: "top",
+             },
+             margin: { l: 0, r: 0, t: 50, b: 20 },
+             dragmode: false,
+         };
+
+         const config = {
+             displayModeBar: true,
+             modeBarButtons: [
+                 ["pan2d"],
+                 ["zoomIn2d"],
+                 ["zoomOut2d"],
+                 ["resetScale2d"],
+                 ["toImage"],
+             ],
+             displaylogo: false,
+             responsive: true,
+             editable: false,
+             scrollZoom: true,
+             showTips: false,
+             modeBarButtonsToRemove: ["select2d", "lasso2d"],
+             toImageButtonOptions: {
+                 format: "png",
+                 filename: "PL",
+                 height: 500,
+                 width: 700,
+             },
+
+         };
+
+         const plot = (
+             <Plot
+                 data={plotData}
+                 layout={layout}
+                 config={config}
+                 style={{ width: "90%", margin: "auto", height: "100%" }}
+                 useResizeHandler={true}
+                 autosize={true}
+             />
+         );
+         setPlotComponent(plot);
+     }, [Constraints]);
 
     const handleMethodChange = (newMethod) => {
         // Add any specific logic when the method changes
--- src/Components/PL Components/OldGraph.jsx
+++ src/Components/PL Components/OldGraph.jsx
@@ -1,12 +1,40 @@
-import React, { useEffect } from "react";
-import Plot from "react-plotly.js/factory";
+import React, { useEffect, useState } from "react";
 import { usePLContext } from "../Apps/PLcontext";
 import Graph_Methodes from "./Graph_Methodes";
 import Plot from "react-plotly.js";
+
 const Graph = () => {
     const { Constraints } = usePLContext();
+    const [plotComponent, setPlotComponent] = useState(null);
 
     useEffect(() => {
+        const generateFeasibleRegionVertices = () => {
+            // Placeholder logic for dynamic generation (replace with actual logic)
+            // This could involve solving a system of inequalities to determine vertices
+            return [
+                { x: 0, y: 0 },
+                { x: 5, y: 0 },
+                { x: 0, y: 5 },
+            ];
+        };
+
+        const generateFeasibleRegionTrace = () => {
+            const vertices = generateFeasibleRegionVertices();
+
+            return {
+                type: "scatter",
+                mode: "lines",
+                name: "Feasible Region",
+                x: vertices.map((vertex) => vertex.x),
+                y: vertices.map((vertex) => vertex.y),
+                fill: "toself",
+                fillcolor: "rgba(0,100,80,0.2)",
+                line: {
+                    color: "rgba(255,255,255,0)",
+                },
+            };
+        };
+
         const plotData = Constraints.map((constraint, index) => {
             const { PlusMinus1, PlusMinus2, Value, X1, X2, Operatore } =
                 constraint;
@@ -15,27 +43,73 @@ const Graph = () => {
             return {
                 type: "scatter",
                 mode: "lines",
-                name: `Constraint ${
-                    index + 1
-                }: ${X1}x₁ ${PlusMinus1} ${X2}x₂ ${PlusMinus2} ${operator} ${Value}`,
-                x: [-1, 8],
-                y: [(Value - X1 * -1) / X2, (Value - X1 * 8) / X2],
+                name: `Constraint ${index + 1}`,
+                x: [-1, 10],
+                y:
+                    PlusMinus2 === "+"
+                        ? [
+                              Value - (X1 * -1 + Number(X2) * -1),
+                              Value - (X1 * 8 + Number(X2) * 10),
+                          ]
+                        : PlusMinus2 === "-"
+                        ? [
+                              Value - (X1 * -1 - Number(X2) * -1),
+                              Value - (X1 * 10 - Number(X2) * 10),
+                          ]
+                        : null,
             };
         });
 
+        plotData.push(generateFeasibleRegionTrace());
+
         const layout = {
-            xaxis: { title: "X" },
-            yaxis: { title: "Y" },
+            xaxis: { range: [-1, 10] },
+            yaxis: { range: [-1, 10] },
+            showlegend: true,
+            legend: {
+                x: 0.1,
+                y: 1.1,
+                xanchor: "center",
+                yanchor: "top",
+            },
+            margin: { l: 0, r: 0, t: 50, b: 20 },
+            dragmode: false,
         };
 
-        return (
+        const config = {
+            displayModeBar: true,
+            modeBarButtons: [
+                ["pan2d"],
+                ["zoomIn2d"],
+                ["zoomOut2d"],
+                ["resetScale2d"],
+                ["toImage"],
+            ],
+            displaylogo: false,
+            responsive: true,
+            editable: false,
+            scrollZoom: true,
+            showTips: false,
+            modeBarButtonsToRemove: ["select2d", "lasso2d"],
+            toImageButtonOptions: {
+                format: "png",
+                filename: "PL",
+                height: 500,
+                width: 700,
+            },
+        };
+
+        const plot = (
             <Plot
                 data={plotData}
                 layout={layout}
-                config={{ displayModeBar: false }}
-                style={{ width: "100%", height: "100%" }}
+                config={config}
+                style={{ width: "90%", margin: "auto", height: "100%" }}
+                useResizeHandler={true}
+                autosize={true}
             />
         );
+        setPlotComponent(plot);
     }, [Constraints]);
 
     const handleMethodChange = (newMethod) => {
@@ -43,11 +117,11 @@ const Graph = () => {
     };
 
     return (
-        <div className="mt-4">
+        <div className="mt-4 pb-20">
             <Graph_Methodes onMethodChange={handleMethodChange} />
             <div className="card mt-4">
                 <div id="constraint-plot" className="card-body">
-                    {/* The Plotly chart will be rendered here */}
+                    {plotComponent}
                 </div>
             </div>
         </div>
