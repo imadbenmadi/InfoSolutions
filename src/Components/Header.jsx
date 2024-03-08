3025a4334cb5dd65053dbe64ddf8bc8fd1cb9af0
imadbenmadi <imadbenmadi891@gmail.com>
2024-03-08 15:43:26 +0100

    Fixing Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,12 +3,16 @@ import logo from "../../public/Cntic_Logo.png";
 
 export default function Header() {
     return (
-        <div className="text-center flex justify-center flex-row w-full mb-[28px]  md:gap-4">
-            <img src={logo} alt="Logo" className=" w-[60px] ml-4 	md:md-5 md:m-0 md:w-[130px]" />
-            <span className="  text-Blue text-2xl font-semibold md:order-2 md:mt-2 md:text-4xl">
-                INFO
+        <div className="text-center flex items-center justify-center flex-row w-full mb-[28px]  md:gap-4">
+            <img
+                src={logo}
+                alt="Logo"
+                className=" w-[60px] ml-4  md:md-5 md:m-0 md:w-[100px]"
+            />
+            <span className="  text-Blue text-2xl font-bold md:order-2 md:mt-2 md:text-4xl">
+                Info
             </span>
-            <span className=" mr-4 text-2xl text-Black font-semibold md:order-3 md:mt-2 md:text-4xl">
+            <span className=" mr-4 text-2xl text-Black font-bold md:order-3 md:mt-2 md:text-4xl">
                 Solutions
             </span>
         </div>
