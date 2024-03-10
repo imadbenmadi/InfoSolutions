c1f4dff0fbfb70d0ac4100ac7749fb373781103f
imadbenmadi <imadbenmadi891@gmail.com>
2024-03-10 22:46:24 +0100

    init PL App

--- src/Apps/PL/PL.jsx
+++ src/Apps/PL/PL.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import React from "react";
+import Page_Title from "../../Components/Page_Title";
+import { PLProvider } from "./PLcontext";
+import Function_Objectif from "./Components/Function_Objectif";
 function PL() {
-  return (
-    <div>PL</div>
-  )
+    return (
+        <div>
+        <Page_Title title={"Programation Lineer"} />
+        <PLProvider>
+            <Function_Objectif />
+        </PLProvider>
+        </div>
+    );
 }
 
-export default PL
+export default PL;
