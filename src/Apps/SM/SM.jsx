497ac7ba7c50054b878e2295525e8aac9b76dc29
khenfer salah eddine <107678289+salahkhenfer@users.noreply.github.com>
2024-03-15 04:04:55 +0100

    FIX

--- src/Apps/SM/SM.jsx
+++ src/Apps/SM/SM.jsx
@@ -1,40 +1,44 @@
 import React, { useState } from "react";
-import Page_Title from "../../Components/Page_Title";
+import PageTitle from "../../Components/Page_Title";
+
 function SM() {
   const [inputValue, setInputValue] = useState("");
-  const [outputValue, setOutputValue] = useState(0);
-  const [inputType, setInputType] = useState("ثنائي");
-  const [outputType, setOutputType] = useState("عشري");
-  const [isNum, setIsNum] = useState(false);
+  const [outputValue, setOutputValue] = useState("0");
+  const [inputType, setInputType] = useState("binary");
+  const [outputType, setOutputType] = useState("decimal");
+  const [isNumValid, setIsNumValid] = useState(true);
 
   const handleInputChange = (e) => {
     const value = e.target.value;
     const regexDecimal = /^[0-9\b]+$/; // Only allow decimal numbers and backspace (\b)
     const regexHex = /^[0-9A-Fa-f\b]+$/; // Only allow hexadecimal numbers and backspace (\b)
 
-    // If the input matches the regex pattern for decimal or hexadecimal or it's an empty string, update the state
-    if (value === "" || regexDecimal.test(value) || regexHex.test(value)) {
-      setIsNum(false);
+    if (
+      value === "" ||
+      (inputType === "binary" && regexDecimal.test(value)) ||
+      (inputType === "hexadecimal" && regexHex.test(value))
+    ) {
+      setIsNumValid(true);
       setInputValue(value);
     } else {
-      setIsNum(true); // Invalid input
+      setIsNumValid(false); // Invalid input
     }
   };
 
   const handleConversion = () => {
     let result;
-    const inputValueParsed = parseInt(inputValue, inputType);
+    const inputValueParsed = parseInt(inputValue, getInputBase());
     switch (outputType) {
-      case "ثنائي":
+      case "binary":
         result = inputValueParsed.toString(2);
         break;
-      case "سداسي عشري":
-        result = inputValueParsed.toString(16);
+      case "hexadecimal":
+        result = inputValueParsed.toString(16).toUpperCase();
         break;
-      case "ثماني":
+      case "octal":
         result = inputValueParsed.toString(8);
         break;
-      case "عشري":
+      case "decimal":
         result = inputValueParsed.toString(10);
         break;
       default:
@@ -43,38 +47,54 @@ function SM() {
     setOutputValue(result);
   };
 
-  const outputTypeOptions = ["ثنائي", "سداسي عشري", "ثماني", "عشري"].filter(
+  const getInputBase = () => {
+    switch (inputType) {
+      case "binary":
+        return 2;
+      case "hexadecimal":
+        return 16;
+      case "octal":
+        return 8;
+      case "decimal":
+        return 10;
+      default:
+        return 10;
+    }
+  };
+
+  const outputTypeOptions = ["ثنائي", "السادس عشري", "ثماني", "عشري"].filter(
     (type) => type !== inputType
   );
 
   const handleRemoveResult = () => {
-    setOutputValue("");
+    setOutputValue("0");
     setInputValue("");
   };
+
   return (
     <div dir="rtl">
-      <Page_Title title="Systeme Machine" />
+      <PageTitle title="Systeme Machine" />
       <div className="flex flex-col items-center justify-center">
-        <div className=" text-center text-xl max-md:flex-col  my-5 flex gap-5">
+        <div className="text-center text-xl max-md:flex-col my-5 flex gap-5">
           <div>
-            <div className="my-1">التحويل من </div>
+            <div className="my-1">تحويل من </div>
             <select
               value={inputType}
               onChange={(e) => setInputType(e.target.value)}
-              className="border   border-gray-500 rounded-md px-2 py-1 mr-2"
+              className="border border-gray-500 rounded-md px-2 py-1 mr-2"
             >
-              <option value="ثنائي">ثنائي</option>
-              <option value="سداسي عشري">سداسي عشري</option>
-              <option value="ثماني">ثماني</option>
-              <option value="عشري">عشري</option>
+              <option value="binary">ثنائي</option>
+              <option value="hexadecimal">السادس عشر</option>
+              <option value="octal">ثماني</option>
+              <option value="decimal">العشري</option>
             </select>
           </div>
           <div>
-            <div className="my-1"> التحويل الى </div>
+            <div className="my-1">التحويل الى </div>
             <select
               value={outputType}
               onChange={(e) => setOutputType(e.target.value)}
-              className="border  border-gray-500 rounded-md px-2 py-1"
+              className="border border-gray-500 rounded-md px-2 py-1"
             >
               {outputTypeOptions.map((type) => (
                 <option key={type} value={type}>
@@ -89,30 +109,23 @@ function SM() {
           value={inputValue}
           onChange={handleInputChange}
           className="border border-gray-500 rounded-md px-2 py-1 mb-2"
-          pattern="[0-9]*" // Only allows numbers
-          inputMode="numeric"
           placeholder="ادخل الرقم هنا"
         />
-        {isNum && (
-          <div className="text-red-500 font-bold"> يجب ان يكون الرقم صالح</div>
-        )}
-        <div className="my-5 text-2xl text-center   font-bold ">
-          {" "}
+        {!isNumValid && <div className="text-red-500 font-bold"> غير صالح</div>}
+        <div className="my-5 text-2xl text-center font-bold">
           النتيجة
-          <div className="min-w-[80%]  h-fit min-h-10 px-5  bg-slate-200">
+          <div className="min-w-[80%] h-fit min-h-10 px-5 bg-slate-200">
             {isNaN(outputValue) ? "0" : outputValue}
           </div>
         </div>
-
-        <div className="flex  max-md:w-[80%] max-md:flex-col gap-2  ">
+        <div className="flex max-md:w-[80%] max-md:flex-col gap-2">
           <button
             disabled={inputValue === ""}
             onClick={handleConversion}
-            className="bg-blue-500 max-md:w-full text-white px-4 py-2 rounded-md "
+            className="bg-blue-500 max-md:w-full text-white px-4 py-2 rounded-md"
           >
             تحويل
           </button>
-
           <button
             onClick={handleRemoveResult}
             className="bg-red-500 text-white px-4 py-2 rounded-md"
