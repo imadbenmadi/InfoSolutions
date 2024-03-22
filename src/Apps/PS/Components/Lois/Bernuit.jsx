17dfdb9c3812fad7ef1da479fa596e6710c15d23
imadbenmadi <imadbenmadi891@gmail.com>
2024-03-23 00:51:26 +0100

    Building Bernuilt applicatiopn

--- src/Apps/PS/Components/Lois/Bernuit.jsx
+++ src/Apps/PS/Components/Lois/Bernuit.jsx
@@ -6,10 +6,11 @@ function Bernuit() {
     const [result, setResult] = useState(null);
     const [expectedValue, setExpectedValue] = useState(null);
     const [variance, setVariance] = useState(null);
+    const [selectedValue, setSelectedValue] = useState(0);
 
     // Function to calculate probability, expected value, and variance
     const calculate = () => {
-        const x = Number(document.getElementById("inputX").value);
+        const x = selectedValue;
         const probability = Math.pow(p, x) * Math.pow(1 - p, 1 - x);
         setResult(probability);
 
@@ -23,7 +24,7 @@ function Bernuit() {
     };
 
     return (
-        <div className="container mx-auto mt-8 px-4">
+        <div className="container mx-auto my-4 px-4">
             <Loi_Header Name="Bernoulli Distribution" />
             <div className="  p-2 border border-gray-300 rounded-lg mb-6 md:w-[50%] m-auto">
                 <div className="italic text-gray-600">
@@ -36,30 +37,61 @@ function Bernuit() {
                     <p>V(X) = p * (1 - p)</p>
                 </div>
             </div>
-            <div className="flex items-center w-[95%] md:w-[50%] m-auto">
-                <label htmlFor="inputX" className="mr-2">
-                    Enter x value (0 or 1):
+            <div className="flex flex-col gap-4 items-center w-[95%] md:w-[50%] m-auto">
+                <label htmlFor="inputX" className="font-semibold md:text-lg ">
+                    choose X value (0 or 1):
                 </label>
-                <input
-                    type="number"
-                    id="inputX"
-                    min="0"
-                    max="1"
-                    defaultValue="0"
-                    className="border p-2 rounded"
-                />
-                <button
-                    onClick={calculate}
-                    className="ml-2 bg-blue-500 text-white px-4 py-2 rounded"
-                >
-                    Calculate
-                </button>
+                <div className=" flex flex-col gap-3">
+                    <div>
+                        <button
+                            onClick={() => setSelectedValue(0)}
+                            className={`${
+                                selectedValue === 0
+                                    ? "bg-blue-500 text-white"
+                                    : "bg-gray-200"
+                            } px-4 py-2 rounded mr-2`}
+                        >
+                            0
+                        </button>
+                        <button
+                            onClick={() => setSelectedValue(1)}
+                            className={`${
+                                selectedValue === 1
+                                    ? "bg-blue-500 text-white"
+                                    : "bg-gray-200"
+                            } px-4 py-2 rounded`}
+                        >
+                            1
+                        </button>
+                    </div>
+
+                    <button
+                        onClick={calculate}
+                        className=" bg-green-500 text-white px-4 py-2 rounded"
+                    >
+                        Calculate
+                    </button>
+                </div>
             </div>
             {result !== null && (
-                <div className="mt-4">
-                    <p>Probability: {result}</p>
-                    <p>Expected Value (E): {expectedValue}</p>
-                    <p>Variance (V): {variance}</p>
+                <div className=" w-[90%] md:w-[30%] m-auto mt-4 md:mt-12 text-lg md:text-xl font-semibold">
+                    <div className="mb-2">
+                        {" "}
+                        <span className=" text-gray-600 ">
+                            Probability:
+                        </span>{" "}
+                        {result}
+                    </div>
+                    <div className="mb-2">
+                        <span className=" text-gray-600 ">
+                            Expected Value (E):
+                        </span>{" "}
+                        {expectedValue}
+                    </div>
+                    <div>
+                        <span className=" text-gray-600 ">Variance (V):</span>{" "}
+                        {variance}
+                    </div>
                 </div>
             )}
         </div>
