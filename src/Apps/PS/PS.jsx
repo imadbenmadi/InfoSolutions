9c28b54f1a6a2e18cb156cbcdddcb6ceb5bc1719
imadbenmadi <imadbenmadi891@gmail.com>
2024-03-22 22:32:47 +0100

    init the application

--- src/Apps/PS/PS.jsx
+++ src/Apps/PS/PS.jsx
@@ -1,11 +1,53 @@
 import React from 'react'
 import Page_Title from '../../Components/Page_Title'
+import { Link } from 'react-router-dom';
 function PS() {
   return (
-    <div> 
-       <Page_Title title="Probability Statistic"/>
-    </div>
-  )
+      <div>
+          <Page_Title title="Probability Statistic" />
+          <div className=" text-xl md:text-3xl text-center w-full font-bold">
+              حلول لقوانين الاحتمالات ذات المتغير المنفصل
+          </div>
+          <div className=" flex flex-col justify-center items-center gap-8  my-6 ">
+              <Link
+                  className=" w-[80%] m-auto bg-gray-200 p-2 md:p-5 text-xl md:text-3xl rounded-lg font-semibold"
+                  to={"/PS/Bernuit"}
+              >
+                  Loi de Bernuit
+              </Link>
+              <Link
+                  className=" w-[80%] m-auto bg-gray-200 p-2 md:p-5 text-xl md:text-3xl rounded-lg font-semibold"
+                  to={"/PS/Benomial"}
+              >
+                  Loi Benomial
+              </Link>
+              <Link
+                  className=" w-[80%] m-auto bg-gray-200 p-2 md:p-5 text-xl md:text-3xl rounded-lg font-semibold"
+                  to={"/PS/Uniforme"}
+              >
+                  Loi Uniforme Discret
+              </Link>
+              <Link
+                  className=" w-[80%] m-auto bg-gray-200 p-2 md:p-5 text-xl md:text-3xl rounded-lg font-semibold"
+                  to={"/PS/Hyper-Geomitrique"}
+              >
+                  Loi Hyper Geomitrique
+              </Link>
+              <Link
+                  className=" w-[80%] m-auto bg-gray-200 p-2 md:p-5 text-xl md:text-3xl rounded-lg font-semibold"
+                  to={"/PS/Geomitrique"}
+              >
+                  Loi Geomitrique
+              </Link>
+              <Link
+                  className=" w-[80%] m-auto bg-gray-200 p-2 md:p-5 text-xl md:text-3xl rounded-lg font-semibold"
+                  to={"/PS/Poissont"}
+              >
+                  Loi de Poissont
+              </Link>
+          </div>
+      </div>
+  );
 }
 
 export default PS
