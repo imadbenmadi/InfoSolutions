92e836da9e7022379e9c2c79777e2c846985d6c0
imadbenmadi <imadbenmadi891@gmail.com>
2024-03-24 23:21:31 +0100

    adding icon

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -23,34 +23,40 @@ function Home() {
   const nav = useNavigate();
 
   return (
-    <motion.div
-      dir="rtl"
-      ref={ref}
-      className=""
-      variants={containerVariants}
-      initial="hidden"
-      animate={isInView ? "visible" : "hidden"}
-    >
-      <motion.div variants={itemVariants} className="md:px-32 p-5">
-        <h1 className="font-bold mx-auto text-3xl text-center py-5 md:p-10 md:text-3xl">
-          {" "}
-          مرحبا بكم{" "}
-        </h1>
-        <h2 className="text-center font-bold  text-xl">
-          المنصة تحتوي على حلول انية لمواد الاعلام الالي{" "}
-        </h2>
-      </motion.div>
-      <div className="grid md:px-32 p-5 md:gap-x-10 min-h-fit  h-fit grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-2">
-        <motion.div variants={itemVariants}>
-          <CardModel shortNameModel="PL" modelNama="Programation Linéer" />
-        </motion.div>
-        <motion.div variants={itemVariants}>
-          <CardModel shortNameModel="PS" modelNama="probability statistics" />
-        </motion.div>
-        <motion.div onClick={() => nav("/SM")} variants={itemVariants}>
-          <CardModel shortNameModel="SM" modelNama="Systéme Machine" />
-        </motion.div>
-        {/* <motion.div variants={itemVariants}>
+      <motion.div
+          dir="rtl"
+          ref={ref}
+          className=""
+          variants={containerVariants}
+          initial="hidden"
+          animate={isInView ? "visible" : "hidden"}
+      >
+          <motion.div variants={itemVariants} className="md:px-32 p-5">
+              <h1 className="font-bold mx-auto text-3xl text-center py-5 md:p-10 md:text-3xl">
+                  {" "}
+                  مرحبا بكم 👋
+              </h1>
+              <h2 className="text-center font-bold  text-xl">
+                  المنصة تحتوي على حلول انية لمواد الاعلام الالي{" "}
+              </h2>
+          </motion.div>
+          <div className="grid md:px-32 p-5 md:gap-x-10 min-h-fit  h-fit grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-2">
+              <motion.div variants={itemVariants}>
+                  <CardModel
+                      shortNameModel="PL"
+                      modelNama="Programation Linéer"
+                  />
+              </motion.div>
+              <motion.div variants={itemVariants}>
+                  <CardModel
+                      shortNameModel="PS"
+                      modelNama="probability statistics"
+                  />
+              </motion.div>
+              <motion.div onClick={() => nav("/SM")} variants={itemVariants}>
+                  <CardModel shortNameModel="SM" modelNama="Systéme Machine" />
+              </motion.div>
+              {/* <motion.div variants={itemVariants}>
           <CardModel shortNameModel="SM" modelNama="Systéme Machine" />
         </motion.div>
         <motion.div variants={itemVariants}>
@@ -59,20 +65,20 @@ function Home() {
         <motion.div variants={itemVariants}>
           <CardModel shortNameModel="SM" modelNama="Systéme Machine" />
         </motion.div> */}
-      </div>
-      <div className="md:px-32 p-5">
-        <h1 className="font-bold mx-auto text-xl text-center py-5 md:p-10 md:text-3xl">
-          {" "}
-          اترك اقتراح لنا
-        </h1>
-        <Beta_Alert />
-        <Feedback />
-      </div>
+          </div>
+          <div className="md:px-32 p-5">
+              <h1 className="font-bold mx-auto text-xl text-center py-5 md:p-10 md:text-3xl">
+                  {" "}
+                  اترك اقتراح لنا
+              </h1>
+              <Beta_Alert />
+              <Feedback />
+          </div>
 
-      {/* how make this platform */}
-      {/* <OurMember /> */}
-      <Footer />
-    </motion.div>
+          {/* how make this platform */}
+          {/* <OurMember /> */}
+          <Footer />
+      </motion.div>
   );
 }
 
